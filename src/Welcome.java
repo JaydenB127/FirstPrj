ff706f86aa504fddb665e37fb216462f515b20f1
thanhdat <bbuidat090@gmail.com>
2024-08-12 16:18:41 +0700

    firstprj

---
+++ src/Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+    public static void main(String[] args) {
+
+        String hoTenSinhVien = "BUI THANH DAT";
+
+
+        String tenLopHocPhan = "DHKHDL19B";
+
+        System.out.println("Hello " + hoTenSinhVien + " và tên lớp học phần là " + tenLopHocPhan);
+    }
+}
